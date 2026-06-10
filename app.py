c06cc5a813fc964bc5839d6a671600d8e470057e
unknown <35680922+pramjati02@users.noreply.github.com>
2026-06-10 16:05:33 +0100

    Reformatted code. Now uses latest langchain 1.3.6, using langchain_classic instead of langchain.chains to keep code up to date. Removed SentenceTransformer embeddings model and instead uses embeddings model that runs directly on Chroma Cloud to potentially avoid errors regarding storage limits on the Render deployment. Retrieval-qa-chat is now used to structure the context and query in a prompt that the LLM can understand

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import SentenceTransformerEmbeddings
-from langchain.chains import RetrievalQA
+from langchain_classic.chains.combine_documents import (
+    create_stuff_documents_chain,
+)
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic import hub
 from langchain_groq import ChatGroq
 import chromadb
 from dotenv import load_dotenv
@@ -18,31 +21,36 @@ os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
  
 class Query(BaseModel):
     input: str
- 
-print("Loading vector store...")
-embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
+# retrieve text format in which LLM processes context and query 
+retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+# API keys for retrieving vector store from Chroma cloud
 client = chromadb.CloudClient(
     api_key=os.environ["CHROMA_API_KEY"],
     tenant=os.environ["CHROMA_TENANT_ID"],
     database=os.environ["CHROMA_DATABASE"]
 )
 
-vectorstore = Chroma(client=client, collection_name="fifa_players", embedding_function=embedding)
+# Retrieve vector store from Chroma cloud, initialize the retrviever, and load LLM
+print("Loading vector store...")
+vectorstore = Chroma(client=client, collection_name="fifa_players")
 retriever = vectorstore.as_retriever(search_kwargs={"k": 6})
- 
+
 print("Loading LLM...")
 llm = ChatGroq(model="llama-3.1-8b-instant")
- 
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    retriever=retriever,
-    chain_type="stuff",
-    return_source_documents=True
+
+# This tells the LLM what the question is and the context from the retriever, formatted in a way that the LLM can understand
+combine_docs_chain = create_stuff_documents_chain(
+    llm, retrieval_qa_chat_prompt
 )
+
+# The retrieval chain which combines the Chroma retriever and the LLM formatted text 
+retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
  
 print("Ready.")
- 
+
+
 # Serve static files (styles.css, script.js)
 app.mount("/static", StaticFiles(directory="static"), name="static")
  
@@ -52,5 +60,5 @@ def serve_frontend():
  
 @app.post("/ask")
 def ask(query: Query):
-    result = qa_chain.invoke({"query": query.input})
-    return {"response": result["result"]}
+    result = retrieval_chain.invoke({"input": query.input})
+    return {"response": result["answer"]}
